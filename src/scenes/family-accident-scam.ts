99c7195f113f4bbddd0cac75fecd8749dd3415b3
Vinlic <vinlic@vinlic.com>
2024-02-15 11:19:04 +0800

    update

--- src/scenes/family-accident-scam.ts
+++ src/scenes/family-accident-scam.ts
@@ -6,7 +6,7 @@ class FamilyAccidentScam extends Scene {
         super({
             id: 'family-accident-scam',
             name: '妈！我出车祸了',
-            description: '常年在外工作的表妹突然给舅妈打来电话：“妈！我出车祸了！要紧急手术，给医院的账户转账...”，听筒中确实是女儿的声音，也许是受了重伤声音有些嘶哑。',
+            description: '常年在外工作的表妹突然给舅妈打来电话：“妈！我出车祸了！要紧急手术，医院收款账号已经发给你了，要先打2万块钱！”，听筒中确实是女儿的声音，也许是受了重伤声音听上去有些嘶哑。',
             coverResId: 'images.scene_family_accident_scam' 
         });
     }
